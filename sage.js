36bb15e2d220a58b694d5b28afa4d60479a46d26
Arthrick <116219066+Arthrick@users.noreply.github.com>
2025-02-12 10:12:19 -0500

    Add files via upload

---
+++ sage.js
@@ -0,0 +1,6 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/GU2XMen1uWIOszrI/scene.splinecode');
+
